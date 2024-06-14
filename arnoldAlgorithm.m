bcedf891c289b3f4716ad0c41f2499109aeac7cf
NixNi <nsntmp@gmail.com>
2024-06-14 15:38:08 +0300

    feat: add columns to table of effiency

--- arnoldAlgorithm.m
+++ arnoldAlgorithm.m
@@ -1,18 +1,3 @@
-
-% % Пример использования функции:
-% input_image = imread('len_std.jpg'); % Загрузка изображения
-% iterations = 0; % Количество итераций алгоритма Арнольда
-% scrambled_image = arnoldAlgorithmStraight(input_image,1,1,1,2, iterations); % Применение алгоритма
-% %imshow(scrambled_image); % Отображение скремблированного изображения
-% 
-% %descrambled_image = arnoldAlgorithmReverse(scrambled_image,1,1,1,2, iterations); % Дешифрование изображения
-% %imshow(descrambled_image); % Отображение дешифрованного изображения
-% 
-% compression_ratio = EntropyMeasure(scrambled_image);
-% disp(['Коэффициент сжатия: ', num2str(compression_ratio)]);
-% 
-
-
 input_image = imread('len_std.jpg'); % Загрузка изображения
 
 max_iterations = 10; % Максимальное количество итераций
@@ -21,10 +6,6 @@ figure('Name', 'Изображения', 'Position', [100, 100, 800, 400]);
 for iterations = 0:max_iterations-1
     % Применение алгоритма с текущим количеством итераций
     scrambled_image = arnoldAlgorithmStraight(input_image,1,1,1,2, iterations);
-
-
-     % Добавление изображения в форму
-    %subplot(2, max_iterations/2, iterations);
     nexttile;
     imshow(scrambled_image);
     title(['Итерация ', num2str(iterations)]);
--- efficiency.m
+++ efficiency.m
@@ -7,8 +7,8 @@ function main()
     folder_path = './saved_images';
     results = process_images(folder_path, a);
 
-    % Вывод результатов в виде таблицы
-    print_results_table(results);
+    % Вывод результатов в виде таблицы и сохранение в Excel
+    print_and_save_results_table(results, 'results.xlsx');
 end
 
 function image_array = load_image(file_path)
@@ -49,6 +49,20 @@ function efficiency = evaluate_efficiency(a, a_prime)
     efficiency = dsf * gsf;
 end
 
+function entropy = calculate_entropy(image)
+    % Загрузка изображения
+    img = image;
+
+    % Методы компрессии
+    initial_size = numel(img);
+    imwrite(img, 'compressed_image.jpg', 'quality', 50);
+    compressed_size = dir('compressed_image.jpg').bytes;
+    entropy = compressed_size / initial_size;
+
+    % Удаление временного файла сжатого изображения
+    delete('compressed_image.jpg');
+end
+
 function results = process_images(folder_path, a)
     results = {};
     image_files = dir(fullfile(folder_path, '*.jpg'));
@@ -57,13 +71,21 @@ function results = process_images(folder_path, a)
         file_name = image_files(i).name;
         file_path_a_prime = fullfile(folder_path, file_name);
         a_prime = load_image(file_path_a_prime);
-        efficiency = evaluate_efficiency(a, a_prime);
+        dsf = calculate_dsf(a, a_prime);
+        gsf = calculate_gsf(a, a_prime);
+        efficiency = dsf * gsf;
+        entropy = calculate_entropy(a_prime);
         results{end+1, 1} = file_name;
         results{end, 2} = efficiency;
+        results{end, 3} = dsf;
+        results{end, 4} = gsf;
+        results{end, 5} = entropy;
     end
 end
 
-function print_results_table(results)
-    headers = {'Prime Image', 'Efficiency'};
-    disp(cell2table(results, 'VariableNames', headers));
+function print_and_save_results_table(results, file_name)
+    headers = {'Prime Image', 'Efficiency', 'DSF', 'GSF', 'Entropy'};
+    results_table = cell2table(results, 'VariableNames', headers);
+    disp(results_table);
+    writetable(results_table, file_name);
 end
